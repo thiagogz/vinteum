7814e46b55f0930e156243ea88a5d8903412a31d
Thiago Zupelli <thiago_zupelli@hotmail.com>
2024-02-06 22:44:21 -0300

    Added class

---
+++ src/main/java/com/example/Carta.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Carta {
+    private int numero;
+    private Naipe naipe;
+    
+    public Carta(int numero, Naipe naipe) {
+        this.numero = numero;
+        this.naipe = naipe;
+    }
+
+    public String imagePath () {
+        return "classic-cards/" + numero + this.naipe + ".png";
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Naipe getNaipe() {
+        return naipe;
+    }
+}
